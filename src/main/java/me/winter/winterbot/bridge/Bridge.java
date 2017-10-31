cbd685010ecab7c1f4f9dcc07aae505cfcff24fd
Alexander Winter <a.w1nter@hotmail.com>
2017-10-30 23:13:15 -0400

    Making Bridge with BitlBee

---
+++ src/main/java/me/winter/winterbot/bridge/Bridge.java
@@ -0,0 +1,67 @@
+package me.winter.winterbot.bridge;
+
+import me.winter.winterbot.command.Command;
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.PircBot;
+
+import java.util.Scanner;
+
+/**
+ * Undocumented :(
+ * <p>
+ * Created by Alexander Winter on 2017-10-30.
+ */
+public class Bridge
+{
+	private final PircBot botA, botB;
+
+	public Bridge(String networkA, final String channelA, String networkB, final String channelB) throws Exception
+	{
+		botA = new PircBot() {
+			{
+				setName("B_55");
+			}
+
+			@Override
+			protected void onMessage(String channel, String sender, String login, String hostname, String message)
+			{
+				botB.sendMessage(channelB, sender + ": " + message);
+			}
+		};
+		//botA.setVerbose(true);
+		botA.connect(networkA);
+		botA.joinChannel(channelA);
+
+
+		botB = new PircBot() {
+			{
+				setName("Bridge_Bitlee_1309120923");
+			}
+
+			@Override
+			protected void onMessage(String channel, String sender, String login, String hostname, String message)
+			{
+				if(sender.equalsIgnoreCase("root"))
+					return;
+
+				botA.sendMessage(channelA, sender + ": " + message);
+			}
+		};
+
+		botB.setVerbose(true);
+		botB.connect(networkB);
+		//botB.sendMessage("&bitlbee", "register psswd");
+		botB.sendMessage("&bitlbee", "identify psswd");
+		//botB.sendMessage("&bitlbee", "account list");
+		//botB.sendMessage("&bitlbee", "account add facebook mail psswd");
+		botB.sendMessage("&bitlbee", "account facebook on");
+		Thread.sleep(10000);
+		botB.sendMessage("&bitlbee", "chat add facebook 1612849258735422 #cem");
+		botB.joinChannel(channelB);
+	}
+
+	public static void main(String[] args) throws Throwable
+	{
+		new Bridge("chat.freenode.net", "#cem", "im.codemonkey.be", "#cem");
+	}
+}
